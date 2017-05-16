a7fe9a8c70a19f51925bd857c18fc942e9c93282
Dennis Campagna <denniscampagna@gmail.com>
2017-05-16 18:17:43 +0200

    Gson example

--- src/main/java/it/polimi/ingsw/parser/JsonToJava.java
+++ src/main/java/it/polimi/ingsw/parser/JsonToJava.java
@@ -1,20 +1,26 @@
 package it.polimi.ingsw.parser;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class JsonToJava {
     public static void main(String[] args) throws IOException {
-        try(Reader reader = new InputStreamReader
-                (JsonToJava.class.getResourceAsStream("/Users/camilladottino/LolloTheMagnificent/src/main/java/it/polimi/ingsw/parser/cardex.json"))){
-            Gson gson = new GsonBuilder().create();
-            CardEx p = gson.fromJson(reader, CardEx.class);
-            System.out.println(p);
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(new CardEx("ciao",2, "ciao"));
+        System.out.println(jsonString);
+
+        Gson gson2 = new Gson();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/cardex.json"));
+            CardEx obj = gson2.fromJson(br, CardEx.class);
+            System.out.println(obj);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
 }
