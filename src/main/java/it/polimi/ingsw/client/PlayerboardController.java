85d4c53510925e88fab4e82037b98a324018a578
Dottins <camilla.dottino@mail.polimi.it>
2017-06-07 17:59:35 +0200

    Playerboard graphics

---
+++ src/main/java/it/polimi/ingsw/client/PlayerboardController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.client;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+
+public class PlayerboardController {
+    @FXML
+    public ImageView card1;
+}
