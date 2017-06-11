8fc13cf0b2caa395cb3cc8e05b32054cc776f6bf
fabri48 <fabrizio.cassetta@mail.polimi.it>
2017-06-11 19:46:02 +0200

    Update client light model

---
+++ src/main/java/it/polimi/ingsw/client/LightModel/PawnColorLight.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.client.LightModel;
+
+public enum PawnColorLight {
+    ORANGE,
+    BLACK,
+    WHITE,
+    NEUTRAL
+}
---
+++ src/main/java/it/polimi/ingsw/client/LightModel/PlayerLight.java
@@ -0,0 +1,96 @@
+package it.polimi.ingsw.client.LightModel;
+
+import it.polimi.ingsw.gamelogic.cards.leader.LeaderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class PlayerLight {
+    private Map<GoodsLight, Integer> myGoods;
+    private List<LeaderCard> myLeaders;
+    private Map<GoodsLight, Integer> myBonusTile;
+    private Map<PawnColorLight, Integer> myPawns;
+    private List<DevelopmentCardLight> myTerritories;
+    private List<DevelopmentCardLight> myBuildings;
+    private List<DevelopmentCardLight> myCharacters;
+    private List<DevelopmentCardLight> myVentures;
+
+    public PlayerLight(Map<GoodsLight, Integer> myGoods,
+                       List<LeaderCard> myLeaders,
+                       Map<GoodsLight, Integer> myBonusTile,
+                       Map<PawnColorLight, Integer> myPawns) {
+        this.myGoods = myGoods;
+        this.myLeaders = myLeaders;
+        this.myBonusTile = myBonusTile;
+        this.myPawns = myPawns;
+        myTerritories = new ArrayList<>();
+        myBuildings = new ArrayList<>();
+        myCharacters = new ArrayList<>();
+        myVentures = new ArrayList<>();
+    }
+
+    public Map<GoodsLight, Integer> getMyGoods() {
+        return myGoods;
+    }
+
+    public void setMyGoods(Map<GoodsLight, Integer> myGoods) {
+        this.myGoods = myGoods;
+    }
+
+    public List<LeaderCard> getMyLeaders() {
+        return myLeaders;
+    }
+
+    public void setMyLeaders(List<LeaderCard> myLeaders) {
+        this.myLeaders = myLeaders;
+    }
+
+    public Map<GoodsLight, Integer> getMyBonusTile() {
+        return myBonusTile;
+    }
+
+    public void setMyBonusTile(Map<GoodsLight, Integer> myBonusTile) {
+        this.myBonusTile = myBonusTile;
+    }
+
+    public Map<PawnColorLight, Integer> getMyPawns() {
+        return myPawns;
+    }
+
+    public void setMyPawns(Map<PawnColorLight, Integer> myPawns) {
+        this.myPawns = myPawns;
+    }
+
+    public List<DevelopmentCardLight> getMyTerritories() {
+        return myTerritories;
+    }
+
+    public void setMyTerritories(List<DevelopmentCardLight> myTerritories) {
+        this.myTerritories = myTerritories;
+    }
+
+    public List<DevelopmentCardLight> getMyBuildings() {
+        return myBuildings;
+    }
+
+    public void setMyBuildings(List<DevelopmentCardLight> myBuildings) {
+        this.myBuildings = myBuildings;
+    }
+
+    public List<DevelopmentCardLight> getMyCharacters() {
+        return myCharacters;
+    }
+
+    public void setMyCharacters(List<DevelopmentCardLight> myCharacters) {
+        this.myCharacters = myCharacters;
+    }
+
+    public List<DevelopmentCardLight> getMyVentures() {
+        return myVentures;
+    }
+
+    public void setMyVentures(List<DevelopmentCardLight> myVentures) {
+        this.myVentures = myVentures;
+    }
+}
