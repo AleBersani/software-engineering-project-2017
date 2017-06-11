52f40855a9922da84b2ab080e4acf7549121ebd5
Dottins <camilla.dottino@mail.polimi.it>
2017-06-11 19:54:49 +0200

    Update light model

---
+++ src/main/java/it/polimi/ingsw/client/LightModel/BoardLight.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.client.LightModel;
+
+public class BoardLight {
+    private
+}
---
+++ src/main/java/it/polimi/ingsw/client/LightModel/SlotLight.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.client.LightModel;
+
+import java.util.Map;
+
+public class SlotLight {
+    private Map<GoodsLight, Integer> bonus;
+    private int cost;
+    private DevelopmentCardLight developmentCard;
+    
+}
