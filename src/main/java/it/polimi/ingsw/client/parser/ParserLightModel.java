9d9263ac7a9ff2891b4d24605356d76601e84907
fabri48 <fabrizio.cassetta@mail.polimi.it>
2017-06-12 01:36:52 +0200

    Updating Parser Client + changements

---
+++ src/main/java/it/polimi/ingsw/client/parser/ParserLightModel.java
@@ -0,0 +1,43 @@
+package it.polimi.ingsw.client.parser;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import it.polimi.ingsw.client.lightmodel.DevelopmentCardLight;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParserLightModel {
+    private ParserSettingsClient parserSettingsClient;
+
+    public ParserLightModel() {
+        parserSettingsClient = new ParserSettingsClient();
+    }
+
+    public List<DevelopmentCardLight> parseDevelopmentCardsClient() throws IOException {
+        Gson gson = new Gson();
+        JsonArray json;
+        JsonObject card;
+        List<String> parsedCosts, parsedPermanentEffectDescription;
+        List<DevelopmentCardLight> parsedCards = new ArrayList<>();
+        json = parserSettingsClient.extractJsonArray("DevelopmentCardsLight.json");
+        for (int i=0; i < json.size(); i++) {
+            card = json.get(i)
+                        .getAsJsonObject();
+            parsedCosts = gson.fromJson(card.get("cost").getAsJsonArray(), new TypeToken<ArrayList<String>>(){}.getType());
+            parsedPermanentEffectDescription = gson.fromJson(card
+                                                                .get("permanentEffectDescription")
+                                                                .getAsJsonArray(),
+                                                            new TypeToken<ArrayList<String>>(){}.getType());
+            parsedCards.add(new DevelopmentCardLight(   card.get("name").getAsString(),
+                                                        parsedCosts,
+                                                        card.get("instantEffectDescription").getAsString(),
+                                                        parsedPermanentEffectDescription));
+        }
+        return parsedCards;
+    }
+
+}
---
+++ src/main/java/it/polimi/ingsw/client/parser/ParserSettingsClient.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.client.parser;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ParserSettingsClient {
+    private static final String PATH = System.getProperty("user.dir") + "/resources/client/json";
+
+    private BufferedReader br = null;
+    private FileReader fr = null;
+
+    public JsonArray extractJsonArray(String jsonName) throws IOException {
+        JsonParser parser = new JsonParser();
+        String path = PATH + jsonName;
+        open(path);
+        JsonArray object = parser.parse(br).getAsJsonArray();
+        close();
+        return object;
+    }
+
+    private void open(String path) throws IOException{
+        fr = new FileReader(path);
+        br = new BufferedReader(fr);
+    }
+
+    private void close() throws IOException {
+        br.close();
+        fr.close();
+    }
+}
