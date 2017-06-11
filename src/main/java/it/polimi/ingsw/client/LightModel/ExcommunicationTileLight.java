f08ce6e9c69af5556127c0c957097422940dd535
Dottins <camilla.dottino@mail.polimi.it>
2017-06-11 19:17:11 +0200

    LightModel package

---
+++ src/main/java/it/polimi/ingsw/client/LightModel/ExcommunicationTileLight.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.client.LightModel;
+
+public class ExcommunicationTileLight {
+    private String name;
+    private String effectDescription;
+
+    public ExcommunicationTileLight(String name, String effectDescription) {
+        this.name = name;
+        this.effectDescription = effectDescription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEffectDescription() {
+        return effectDescription;
+    }
+
+    public void setEffectDescription(String effectDescription) {
+        this.effectDescription = effectDescription;
+    }
+}
