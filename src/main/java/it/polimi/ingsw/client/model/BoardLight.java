9b0d501b580c45d142bb1c5af7ee249958229300
Dottins <camilla.dottino@mail.polimi.it>
2017-07-05 17:49:18 +0200

    Test
    Signed-off-by: Dottins <camilla.dottino@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/client/model/BoardLight.java
+++ src/main/java/it/polimi/ingsw/client/model/BoardLight.java
@@ -12,7 +12,81 @@ public class BoardLight {
     private List<SlotLight> market;
     private CouncilPalaceLight councilPalaceLight;
 
-    public BoardLight() {
-        // TODO
+    public BoardLight(List<TowerSlotLight> greenTower, List<TowerSlotLight> yellowTower,
+                      List<TowerSlotLight> blueTower, List<TowerSlotLight> purpleTower,
+                      List<SlotLight> production, List<SlotLight> harvest,
+                      List<SlotLight> market, CouncilPalaceLight councilPalaceLight) {
+        this.greenTower = greenTower;
+        this.yellowTower = yellowTower;
+        this.blueTower = blueTower;
+        this.purpleTower = purpleTower;
+        this.production = production;
+        this.harvest = harvest;
+        this.market = market;
+        this.councilPalaceLight = councilPalaceLight;
+    }
+
+    public List<TowerSlotLight> getGreenTower() {
+        return greenTower;
+    }
+
+    public void setGreenTower(List<TowerSlotLight> greenTower) {
+        this.greenTower = greenTower;
+    }
+
+    public List<TowerSlotLight> getYellowTower() {
+        return yellowTower;
+    }
+
+    public void setYellowTower(List<TowerSlotLight> yellowTower) {
+        this.yellowTower = yellowTower;
+    }
+
+    public List<TowerSlotLight> getBlueTower() {
+        return blueTower;
+    }
+
+    public void setBlueTower(List<TowerSlotLight> blueTower) {
+        this.blueTower = blueTower;
+    }
+
+    public List<TowerSlotLight> getPurpleTower() {
+        return purpleTower;
+    }
+
+    public void setPurpleTower(List<TowerSlotLight> purpleTower) {
+        this.purpleTower = purpleTower;
+    }
+
+    public List<SlotLight> getProduction() {
+        return production;
+    }
+
+    public void setProduction(List<SlotLight> production) {
+        this.production = production;
+    }
+
+    public List<SlotLight> getHarvest() {
+        return harvest;
+    }
+
+    public void setHarvest(List<SlotLight> harvest) {
+        this.harvest = harvest;
+    }
+
+    public List<SlotLight> getMarket() {
+        return market;
+    }
+
+    public void setMarket(List<SlotLight> market) {
+        this.market = market;
+    }
+
+    public CouncilPalaceLight getCouncilPalaceLight() {
+        return councilPalaceLight;
+    }
+
+    public void setCouncilPalaceLight(CouncilPalaceLight councilPalaceLight) {
+        this.councilPalaceLight = councilPalaceLight;
     }
 }
